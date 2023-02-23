a3606e28e42ce2d207e1195287526b211ed2f6a2
Michael Thompson <clonenum3@users.noreply.github.com>
2023-02-22 19:55:01 -0500

    Update ceph.pp
    migrating to globus v5

--- manifests/ceph.pp
+++ manifests/ceph.pp
@@ -11,29 +11,28 @@ class globus::ceph (
     String $process_user = 'globus-ceph',
     Integer $process_threads = 2
 ) {
+###     accounts::user { "${process_user}":
+###         ensure => $ensure
+###     }
 
-    accounts::user { "${process_user}":
-        ensure => $ensure
-    }
+###     package { 'globus-gridftp-server-ceph': 
+###         ensure => $ensure,
+###         require => Package['globus-connect-server']
+###     }
 
-    package { 'globus-gridftp-server-ceph': 
-        ensure => $ensure,
-        require => Package['globus-connect-server']
-    }
+###     file { '/etc/gridftp.d/gridftp-ceph':
+###         content => template('globus/gridftp-ceph.erb'),
+###         ensure => $ensure,
+###         tag => [ 'globus-config' ]
+###      }
 
-    file { '/etc/gridftp.d/gridftp-ceph':
-        content => template('globus/gridftp-ceph.erb'),
-        ensure => $ensure,
-        tag => [ 'globus-config' ]
-     }
-
-     file { '/etc/globus/globus-gridftp-server-ceph.conf':
-        content => template('globus/globus-gridftp-server-ceph.conf.erb'),
-        ensure => $ensure,
-        owner => 'root',
-        group => 'root',
-        mode => '0600',
-        tag => [ 'globus-config' ],
-        require => Package['globus-gridftp-server-ceph']
- }
+###      file { '/etc/globus/globus-gridftp-server-ceph.conf':
+###         content => template('globus/globus-gridftp-server-ceph.conf.erb'),
+###         ensure => $ensure,
+###         owner => 'root',
+###         group => 'root',
+###         mode => '0600',
+###         tag => [ 'globus-config' ],
+###         require => Package['globus-gridftp-server-ceph']
+###      }
 }
